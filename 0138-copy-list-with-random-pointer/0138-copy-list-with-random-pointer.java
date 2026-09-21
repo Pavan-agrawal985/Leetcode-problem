c70ca21d573e4e635e5701626e0961eaf72f2a22
Pavan@Agrawal <pavanagrawal698@gmail.com>
2026-09-21 20:38:11 +0530

    Time: 0 ms (100%), Space: 46.5 MB (72.13%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,24 +15,58 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        // Node temp=head;
+        // HashMap<Node,Node> map=new HashMap<>();
+        // while(temp!=null){
+        //     Node newnode=new Node(temp.val);
+        //     map.put(temp,newnode);
+        //     temp=temp.next;
+            
+            
+        // }
+        // temp=head;
+        // while(temp!=null){
+        //     Node copynode=map.get(temp);
+        //     copynode.next=map.get(temp.next);
+        //     copynode.random=map.get(temp.random);
+        //     temp=temp.next;
+        // }
+        // return map.get(head);
+
+
+
         Node temp=head;
-        HashMap<Node,Node> map=new HashMap<>();
+
         while(temp!=null){
             Node newnode=new Node(temp.val);
-            map.put(temp,newnode);
-            temp=temp.next;
-            
+            newnode.next=temp.next;
+            temp.next=newnode;
+            temp=temp.next.next;
             
         }
         temp=head;
         while(temp!=null){
-            Node copynode=map.get(temp);
-            copynode.next=map.get(temp.next);
-            copynode.random=map.get(temp.random);
-            temp=temp.next;
+            Node copynode=temp.next;
+            if(temp.random!=null){
+                copynode.random=temp.random.next;
+            }
+            else{
+                copynode.random=null;
+            }
+            
+            temp=temp.next.next;
         }
-        return map.get(head);
+        Node dummy=new Node(-1);
+        Node res=dummy;
+        temp=head;
+        while(temp!=null){
+            res.next=temp.next;
+            temp.next=temp.next.next;
+            temp=temp.next;
+            res=res.next;
+        }
 
+        return dummy.next;
         
 
         
