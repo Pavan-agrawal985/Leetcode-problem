7219d6b7c64adb8468f44cde50e2986b1be1a30b
Pavan@Agrawal <pavanagrawal698@gmail.com>
2026-09-24 10:22:27 +0530

    Time: 1 ms (77.7%), Space: 42.4 MB (50.38%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,30 +1,32 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        if(n==1) return true;
-        int r=n;
-        while(r!=1){
+        HashSet<Integer> set=new HashSet<>();
+        
+        while(n!=1){
+            if(set.contains(n)){
+                return false;}
+
+                set.add(n);
+
+
+
+                
             int ss=0;
-            int digit=r;
+            int digit=n;
             while(digit>0){
                 ss+=(digit % 10) * (digit % 10);
                 digit/=10;
             }
-            if(ss==1){
-                return true;
-            }
-            if(map.containsKey(r)){
-                return false;
-            }else{
-                map.put(r,ss);
-            }
-            r=ss;
+            
+            
+            
+            n=ss;
             
 
 
         }
 
-        return false;
+        return true;
 
         
         
