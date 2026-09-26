b6f8a0b78b46c36d77e31a1607cd2d15989a07ac
Pavan@Agrawal <pavanagrawal698@gmail.com>
2026-09-26 07:40:45 +0530

    Time: 2 ms (86.27%), Space: 49.2 MB (53.68%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,31 +9,51 @@
  * }
  */
 class Solution {
+    public ListNode Middle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast.next!=null && fast.next.next !=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return slow;
+    }
+    public ListNode reverse(ListNode head){
+        if(head==null || head.next== null) return head;
+        ListNode prev=null;
+        ListNode temp=head;
+        while(temp!=null){
+            ListNode front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
     public void reorderList(ListNode head) {
+        ListNode middle=Middle(head);
+        ListNode reversed=reverse(middle.next);
+        middle.next=null;
+         
+        ListNode temp=head;
+        ListNode temp1=reversed;
+
+        while(temp1!=null){
+           ListNode front1 = temp.next;
+            ListNode front2 = temp1.next;
+
+            temp.next = temp1;
+            temp1.next = front1;
+
+            temp = front1;
+            temp1 = front2;
         
-       
-        ListNode x=head;
-       
-
-        while(x!=null && x.next!=null){
-            ListNode y=x;
-            while(y.next!=null &&y.next.next!=null){
-                y=y.next;
-            }
-            if(x==y){
-                break;
-            }
-            ListNode last=y.next;
-            
-            
-            y.next=null;
-            last.next=x.next;
-            x.next=last;
-            x=last.next;
-
         }
 
 
+       
         
         
     }
